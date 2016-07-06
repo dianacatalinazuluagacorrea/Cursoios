8cc23055135b7196e6506dda66f0f1f891061247
Catalina Zuluaga <catalinazuluaga@macbook-pro-de-catalina.local>
2016-07-05 21:22:19 -0500

    Initial Commit

---
+++ ultimaTests/ultimaTests.swift
@@ -0,0 +1,36 @@
+//
+//  ultimaTests.swift
+//  ultimaTests
+//
+//  Created by Catalina Zuluaga on 5/07/16.
+//  Copyright © 2016 Catalina Zuluaga. All rights reserved.
+//
+
+import XCTest
+@testable import ultima
+
+class ultimaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
